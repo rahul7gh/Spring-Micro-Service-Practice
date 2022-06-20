0145df564a0ae33c2843ab7bcdbf390b30b2bcac
Rahul Pohare <rahulpohare41@gmail.com>
2022-06-20 21:25:51 +0530

    Added configrepo entry for service discovery

--- src/main/java/com/optimagrowth/OstockConfigServerApplication.java
+++ src/main/java/com/optimagrowth/OstockConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SpringMicroservicesApplication {
+public class OstockConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMicroservicesApplication.class, args);
+		SpringApplication.run(OstockConfigServerApplication.class, args);
 	}
 
 }
